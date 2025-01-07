39261e5adf68147c5bd5ac2ef69ad7e8b4be94d5
phi.vo <phi.vo@agest.vn>
2025-01-07 16:41:57 +0700

    1111

--- my-python-tests/python_test_4.py
+++ my-python-tests/python_test_4.py
@@ -16,7 +16,7 @@ class Hello():
                 envs = (yaml.safe_load(stream))
                 print(envs['env']['TOKEN'])
                 print(envs['env']['TOKEN_2'])
-                print(os.getenv('MY_API_KEY'))
+                print(os.getenv('MY_SECRET_API_KEY'))
                 print(os.getenv('MY_SECRET_PASSWORD'))
             except yaml.YAMLError as exc:
                 print(exc)
